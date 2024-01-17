2beddf32dfd374fb1262082926b0c8289178191c
Chinmoy Chakraborty <cchakraborty@microsoft.com>
2024-01-17 22:07:55 +0000

    Added HomeController.cs to /Controllers

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dotnetmvc.Models;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+namespace dotnetmvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ActionName("Index")]
+    public async Task<IActionResult> Ask(AskQuestion q)
+    {
+        var question = Request.Form["question"];
+        // string url = "http://localhost:7071/api/MyChatFunction";
+        string url = "https://ragchatfunc.azurewebsites.net/api/MyChatFunction?code=JO9dslnIm5VksWlFmr0MaaWIcWzFTkjt74GTRcUB0SQcAzFuhCB8KQ==";
+
+        Console.WriteLine("Question from the model :: " + q.Question);
+        ModelState.Clear();
+
+        using HttpClient client = new();
+
+        HttpResponseMessage response = await client.PostAsync(
+            requestUri: url,
+            content: new StringContent(question, Encoding.UTF8, "text/plain"));
+
+        var answer = await response.Content.ReadAsStringAsync();
+        q.Answer = answer;
+        q.Question = "";
+
+        Console.WriteLine($"\nAI: {answer}");
+
+        return View(q);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
